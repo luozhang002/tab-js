3d505aed1fc44f01e6fdec92da4ce76ffc71baba
luozhang002 <luozhang002@126.com>
2016-07-11 11:04:15 +0800

    add new tab file include html css javascript

---
+++ js/tab.js
@@ -0,0 +1,31 @@
+var tabs=document.getElementById("tab").getElementsByTagName("li");
+
+var divs=document.getElementById("tab_con").getElementsByTagName("div");
+
+for(var i=0;i<tabs.length;i++){
+
+tabs[i].onclick=function(){change(this);}
+
+}
+
+function change(obj){
+
+	for(var i=0;i<tabs.length;i++){
+
+		if(tabs[i]==obj){
+
+		tabs[i].className="fli";
+
+		divs[i].className="fdiv";
+
+	}else{
+
+		tabs[i].className="";
+
+		divs[i].className="";
+
+		}
+
+	}
+
+}
